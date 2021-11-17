b2b455a6bba056114ed5f7c204d517349de24ea9
hushuo <1253740807@qq.com>
2021-11-17 16:44:51 +0800

    topnav样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
         <div class = "topnav">
-            <div class = "logo"></div>
-            <div class = "menu"></div>
+            <div class = "logo">LOGO</div>
+            <ul class = "menu">
+                <li>菜单1</li>
+                <li>菜单2</li>
+            </ul>
         </div>
-        <div class="banner">
+        <div class="banner"> 
             <h1>轱辘UI</h1>
             <h2>一个厉害地 UI 框架</h2>
             <p class="actions">
@@ -16,5 +19,21 @@
 </template>
 
 <style lang="scss" scoped>
-    .topnav{border: 1px solid red;}
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
 </style>
