aad8913ad8dae5b055b2eebf943767b06ffbbb0f
hushuo <1253740807@qq.com>
2021-11-17 15:46:26 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Hello  、、 
+  </div>
+  <Hushuo/>
+
+</template>
+
+<script>
+import Hushuo from './components/Hushuo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Hushuo: Hushuo
+  }
+}
+</script>
