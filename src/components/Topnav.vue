f3d91e90e47309537d7c8189ac3253d1809cf26a
hushuo <1253740807@qq.com>
2021-11-19 17:35:15 +0800

    从iconfont.cn引入图标

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,9 @@
 <template>
     <div class = "topnav">
-        <div class = "logo" @click="toggleMenu">LOGO</div>
+        <div class = "logo">
+          <svg class="icon">
+    <use xlink:href="#icon-lemon"></use>
+</svg></div>
         <ul class = "menu">
             <li>菜单1</li>
             <li>菜单2</li>
@@ -22,7 +25,9 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+$color: #007974;
 .topnav {
+  color: $color;
   display: flex; 
   padding: 16px;
   position: fixed;
@@ -35,6 +40,11 @@ export default {
   > .logo {
     max-width: 6em;
     margin-right: auto;
+    > svg {
+      width: 32px;
+      height: 32px;
+      color: rgba(255, 38, 0, 0.897);
+    }
   }
   > .menu {
     display: flex;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
     <div class="topnavAndBanner">
         <Topnav />
         <div class="banner"> 
@@ -10,6 +11,19 @@
             </p>
         </div>
     </div>
+    <div class="features">
+      <svg class="icon" aria-hidden="true">
+      <use xlink:href="#icon-Vue"></use>
+      </svg>
+      <svg class="icon" aria-hidden="true">
+      <use xlink:href="#icon-ts"></use>
+      </svg>
+      <svg class="icon" aria-hidden="true">
+      <use xlink:href="#icon-lights"></use>
+      </svg>
+    </div>
+
+  </div>
 </template>
 <script lang="ts">
 import Topnav from "../components/Topnav.vue";
@@ -20,11 +34,21 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+$green: #02bcb0;
+$borde-radius: 4px;
+$color: #007974;
 .topnavAndBanner {
   background: rgb(227,255,253);
   background-color: liner-gradient(145deg, rgba(227,225,253,1) 0%, rgba(183,233,230,1) 100%);
 }
+.features {
+  > svg {
+    width: 64px;
+    height: 64px;
+  }
+}
 .banner {
+  color: $color;
   padding: 100px 0;
   display: flex;
   justify-content: center;
@@ -34,13 +58,14 @@ export default {
     padding: 8px 0;
     a {
       margin:0 8px;
-      background: #fff;
+      background: $green;
+      color:white;
       display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h*0.5;
-      padding: 0 8px;
+      padding: 8px 24px;
+      border-radius: $borde-radius;
+      &:hover {
+        text-decoration: none;
+      }
     }
   }
 }
