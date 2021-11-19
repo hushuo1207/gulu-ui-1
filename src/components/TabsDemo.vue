503bf449cf45bbbb7ac4ad5f7f453babd47af976
hushuo <1253740807@qq.com>
2021-11-19 16:16:20 +0800

    增加tabs利用插槽的特性连接不同的内容

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -5,18 +5,28 @@
         <Tab title="导航1">内容1</Tab>
         <Tab title="导航2222222">内容2</Tab>
     </Tabs>
+    <div></div>
+
+    <div>Tabs示例</div>
+    <h1>示例2</h1>
+    <Tabs2 v-model:selected2="y">
+        <Tab title="导航1">内容1</Tab>
+        <Tab title="导航2">内容2</Tab>
+    </Tabs2>
 
 
 </template>
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue'
+import Tabs2 from '../lib/Tabs2.vue'
 import Tab from '../lib/Tab.vue'
 import { ref } from 'vue'
 export default {
-    components: { Tabs, Tab },
+    components: { Tabs, Tab, Tabs2 },
     setup() {
         const x = ref('导航1')
-        return {x}
+        const y = ref('导航1')
+        return {x, y}
     }
 }
 </script>
