02d0114a9cb59cd55e936cc1fd47c2ac6e59156f
hushuo <1253740807@qq.com>
2021-11-19 17:44:02 +0800

    圆弧实现

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -39,7 +39,10 @@ $borde-radius: 4px;
 $color: #007974;
 .topnavAndBanner {
   background: rgb(227,255,253);
-  background-color: liner-gradient(145deg, rgba(227,225,253,1) 0%, rgba(183,233,230,1) 100%);
+  background: liner-gradient(145deg, rgba(227,225,253,1) 0%, rgba(183,233,230,1) 100%);
+  // border-bottom-left-radius: 50% 40px;
+  // border-bottom-right-radius: 50% 40px;
+  clip-path: ellipse(80% 60% at 50% 40%);
 }
 .features {
   > svg {
