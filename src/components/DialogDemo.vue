723b783b175a30e35441cba7827926758fbdd792
hushuo <1253740807@qq.com>
2021-11-20 16:33:33 +0800

    tab 、 dialog 代码改造

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,59 +1,23 @@
 <template>
-    <div>Dialog 示例</div>
-    <h1>示例1</h1>
-    <Button @click="toggle">toggle</Button>
-    <Dialog v-model:visiable="x" :closeOnClickOverlay="false"
-      :ok="f1" :cancel="f2">
-        <template v-slot:content>
-          <strong>嗨1</strong>
-          <div>
-            嗨2
-          </div>
-        </template>
-        <template v-slot:title>
-          <strong>标题</strong>
-        </template>
-    </Dialog>
-    <h1>示例2</h1>
-    <Button @click="showDialog">show</Button>
-
-    <!-- <Dialog ></Dialog> -->
+    <h2>Dialog 示例</h2>
+  <Demo :component="Dialog1Demo" />
+  <Demo :component="Dialog2Demo" />
 </template>
 
 <script lang="ts">
-import Dialog from '../lib/Dialog.vue'
-import Button from '../lib/Button.vue'
-import { ref, h } from 'vue'
-import { openDialog } from '../lib/openDialog'
+import Demo from './Demo.vue';
+import Dialog1Demo from './Dialog1.demo.vue';
+import Dialog2Demo from './Dialog2.demo.vue';
 
 export default {
   components: {
-    Dialog,
-    Button
+    Demo
   },
   setup() {
-    const x = ref(false)
-    const toggle = () => {
-      x.value = !x.value
+    return {
+      Dialog1Demo,
+      Dialog2Demo
     }
-    const f1 = () => {
-      return false
-    }
-    const f2 = () => {
-    }
-    const showDialog = () => {
-      openDialog({
-        title: '标题',
-        content: '你好',
-        ok(){
-          console.log('ok')
-        },
-        cancel(){
-          console.log('cancel')
-        }
-      })
-    }
-    return {x, toggle, f1, f2, showDialog}
   }
 }
 </script>
--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,6 +1,11 @@
 <template>
+
+<h2>Tabs 示例</h2>
+<Demo :component="Tabs1Demo" />
+<hr />
+
     <div>Tabs示例</div>
-    <h1>示例1</h1>
+    <h2>示例1</h2>
     <Tabs v-model:selected="x">
         <Tab title="导航1">内容1</Tab>
         <Tab title="导航2222222">内容2</Tab>
@@ -8,7 +13,7 @@
     <div class="gulu-fen"></div>
 
     <div>Tabs示例</div>
-    <h1>示例2</h1>
+    <h2>示例2</h2>
     <Tabs2 v-model:selected2="y">
         <Tab title="导航1">内容1</Tab>
         <Tab title="导航2222">内容2</Tab>
@@ -17,16 +22,19 @@
 
 </template>
 <script lang="ts">
+import Demo from './Demo.vue'
+import Tabs1Demo from './Tabs1.demo.vue'
+
 import Tabs from '../lib/Tabs.vue'
 import Tabs2 from '../lib/Tabs2.vue'
 import Tab from '../lib/Tab.vue'
 import { ref } from 'vue'
 export default {
-    components: { Tabs, Tab, Tabs2 },
+    components: { Tabs, Tab, Tabs2 , Demo},
     setup() {
         const x = ref('导航1')
         const y = ref('导航1')
-        return {x, y}
+        return {x, y, Tabs1Demo}
     }
 }
 </script>
